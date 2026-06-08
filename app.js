d6e043d5f6450713217cc4c2037624cc361ef0fb
DanielChernov99 <chernov.dani@gmail.com>
2026-06-08 20:14:40 +0300

    added somethig small

--- app.js
+++ app.js
@@ -2,3 +2,5 @@ const contactService = require("./services/contactService")
 const { handleCommand } = require("./commands/commandHandler")
 
 const [,,command,...args] = process.argv
+
+handleCommand(command,args)
