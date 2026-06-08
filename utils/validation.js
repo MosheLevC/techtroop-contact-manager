2eadf3674ab6cc6b3d69d1931c16895965803420
Moshe Lev Ceder <navkc11@gmail.com>
2026-06-08 21:19:25 +0300

    feat: update command validation to return structured response with validity status

--- utils/validation.js
+++ utils/validation.js
@@ -11,33 +11,51 @@ export const validateCommand = (command, args) => {
   switch (command) {
     case "add":
       if (args.length === 3 && testFullNameRegex(args[0]) && testEmailRegex(args[1]) && testPhoneRegex(args[2])) {
-        return true;
+        return { isValid: true };
       }
-      return false;
+      return {
+        isValid: false,
+        message:
+          args.length > 3
+            ? "too many arguments"
+            : args.length < 3
+              ? "missing arguments"
+              : !testFullNameRegex(args[0])
+                ? "invalid full name: " + args[0]
+                : !testEmailRegex(args[1])
+                  ? "invalid email: " + args[1]
+                  : "invalid phone: " + args[2],
+      };
       break;
     case "list":
       if (args.length === 0) {
-        return true;
+        return { isValid: true };
       }
-      return false;
+      return { isValid: false, message: "too many arguments" };
       break;
     case "delete":
       if (args.length === 1 && testEmailRegex(args[0])) {
-        return true;
+        return { isValid: true };
       }
-      return false;
+      return {
+        isValid: false,
+        message: args.length > 1 ? "too many arguments" : args.length < 1 ? "missing arguments" : "invalid email: " + args[0],
+      };
       break;
     case "search":
       if (args.length === 1 && (testFullNameRegex(args[0]) || testEmailRegex(args[0]))) {
-        return true;
+        return { isValid: true };
       }
-      return false;
+      return {
+        isValid: false,
+        message: args.length > 1 ? "too many arguments" : args.length < 1 ? "missing arguments" : "invalid full name/email: " + args[0],
+      };
       break;
     default:
       if (args.length === 0) {
-        return true;
+        return { isValid: true };
       }
-      return false;
+      return { isValid: false, message: "too many arguments" };
       break;
   }
 };
