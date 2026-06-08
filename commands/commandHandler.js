95a12b67021cabdbcceb5c6734128224d3d6539a
Moshe Lev Ceder <navkc11@gmail.com>
2026-06-08 21:54:58 +0300

    feat: add success and error printing functions for command handling

--- commands/commandHandler.js
+++ commands/commandHandler.js
@@ -3,3 +3,16 @@ import { validateCommand } from "../utils/validation.js";
 export const handleCommand = (command, args) => {
   return validateCommand(command, args);
 };
+
+export const printSuccess = (commandResult) => {
+  console.log("✅ success!");
+  if (commandResult?.message) {
+    console.log(commandResult?.message);
+  } else {
+    console.log(commandResult);
+  }
+};
+
+export const printError = (error) => {
+  console.log("⛔ Error:", error);
+};
